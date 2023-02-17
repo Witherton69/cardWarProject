526afbd0ac1a455702c599b3a7a06e91a8350b4e
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-02-17 09:10:58 -0600

    Spaced out stuff and made it look all fancy

--- cardWarProject/ContentView.swift
+++ cardWarProject/ContentView.swift
@@ -13,28 +13,43 @@ struct ContentView: View {
             Image("background")
             
             VStack {
+                Spacer()
                 Image("logo")
                 
+                Spacer()
+                
                 HStack {
+                    Spacer()
                     Image("card5")
+                    Spacer()
                     Image("card5")
+                    Spacer()
                 }
+                
+                Spacer()
+                
                 Button {
                     print("Button Clicked")
                 } label: {
                     Image("dealbutton")
                 }
                 
+                Spacer()
+                
                 HStack {
+                    Spacer()
                     VStack {
                         Text("BabyGirl")
                         Text("0")
                     }
+                    Spacer()
                     VStack {
                         Text("Kitten")
                         Text("0")
                     }
+                    Spacer()
                 }
+                Spacer()
             }
         }
     }
