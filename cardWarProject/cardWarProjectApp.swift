6f3099b8f3922eb45eb1c70a5047e90c7add30fa
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-02-17 08:27:46 -0600

    Initial Commit

---
+++ cardWarProject/cardWarProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardWarProjectApp.swift
+//  cardWarProject
+//
+//  Created by Katelyn Gosnell on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct cardWarProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
