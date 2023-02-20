500e525add2843844d36428cafbfa5b5715edb45
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-02-20 09:21:15 -0600

    The final thing. So long babygirl and kitten. You will be missed

--- cardWarProject/ContentView.swift
+++ cardWarProject/ContentView.swift
@@ -8,28 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    // State values/variables
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    @State var playerCard = "card2"
+    @State var cpuCard = "card2"
+    
     var body: some View {
         ZStack {
             Image("background")
             
             VStack {
                 Spacer()
+                
                 Image("logo")
                 
                 Spacer()
                 
                 HStack {
                     Spacer()
-                    Image("card5")
+                    // Displays a card image based on the current value of the playerCard state variable
+                    Image(playerCard)
                     Spacer()
-                    Image("card5")
+                    // Displays a card image based on the current value of the cpuCard state variable
+                    Image(cpuCard)
                     Spacer()
                 }
                 
                 Spacer()
                 
                 Button {
-                    print("Button Clicked")
+                    // The following code will run each time the button is clicked
+                    
+                    // The random method generates a random Int from one specified Int to a second specified Int (Ex: Int.random(in: Int...14)
+                    let playerRandom = Int.random(in: 2...14)
+                    let cpuRandom = Int.random(in: 2...14)
+                    
+                    // Updates the cards displayed on the app
+                    playerCard = "card" + String(playerRandom)
+                    cpuCard = "card" + String(cpuRandom)
+
+                    // Update the score
+                    if (playerRandom > cpuRandom){
+                        playerScore = playerScore + 1
+                    } else if (playerRandom < cpuRandom) {
+                        cpuScore = cpuScore + 1
+                    }
+
                 } label: {
                     Image("dealbutton")
                 }
@@ -39,27 +65,26 @@ struct ContentView: View {
                 HStack {
                     Spacer()
                     VStack {
-                        Text("Babygirl")
-                            .font(
-                            .headline)
-                            .foregroundColor(Color
-                            .white)
+                        // Styling can added to elements/views by using the right panel
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
-                        Text("0")
+
+                        Text(String(playerScore))
                             .font(.largeTitle)
-                            .foregroundColor(Color .white)
+                            .foregroundColor(Color.white)
                     }
                     Spacer()
                     VStack {
-                        Text("Kitten")
-                            .font(
-                            .headline)
-                            .foregroundColor(Color
-                            .white)
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
-                        Text("0")
+
+                        Text(String(cpuScore))
                             .font(.largeTitle)
-                            .foregroundColor(Color .white)
+                            .foregroundColor(Color.white)
                     }
                     Spacer()
                 }
